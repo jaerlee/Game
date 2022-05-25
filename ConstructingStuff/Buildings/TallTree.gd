498aac111d4ad3d896c7d1162858add9db422a63
BazDeVaul <104587892+BazDeVaul@users.noreply.github.com>
2022-05-25 16:35:43 -0400

    Add files via upload
    This includes changes to the tree summoner script, the tall tree script, the tile sets, the enemy outpost scene, and a new script for the level loader

--- ConstructingStuff/Buildings/TallTree.gd
+++ ConstructingStuff/Buildings/TallTree.gd
@@ -3,15 +3,24 @@ extends Area2D
 export var height_change = Vector2()
 var height
 var TreeSprite = preload("res://ConstructingStuff/ForTree/TreeSprite.tscn")
-var new_TreeSprite
+var EnemySpot = preload("res://ConstructingStuff/Buildings/TrunkWithOutpost.tscn")
+var new_Sprite
+onready var HitBox = get_child(0)
 
 func _ready():
-	for n in height:
-		new_TreeSprite = TreeSprite.instance()
-		add_child(new_TreeSprite)
-		new_TreeSprite.global_position = global_position + height_change*n
 	global_position = get_parent().global_position
-	print(global_position,"TreePosition")
-	print(height,"TreeHeight")
+	for n in (height-1):
+		randomize()
+		if randi()%5 == 4:
+			new_Sprite = EnemySpot.instance()
+		else:
+			new_Sprite = TreeSprite.instance()
+		add_child(new_Sprite)
+		get_child(n+2).global_position = get_child(n+1).global_position + height_change*5
+#		print(get_child(n+2).global_position, "SpriteHeight")
+	HitBox.scale = Vector2(1,height)
+	HitBox.translate((height-1) * height_change/2)
+	
+	
 	
 
--- ConstructingStuff/NewTiles/TileScripts/Fort.gd
+++ ConstructingStuff/NewTiles/TileScripts/Fort.gd
@@ -2,5 +2,3 @@ extends TileMap
 
 export var PositionChange = Vector2()
 
-func _ready():
-	print(global_position, "Forest")
