2c18f8d6a94019ddd7aa6d1cc49f91818f8804c1
BazDeVaul <104587892+BazDeVaul@users.noreply.github.com>
2022-05-31 17:15:01 -0400

    New Constructing Stuff, Enemy Folder, and Sprites

--- ConstructingStuff/ObjectSummoners/TreeSummoner.gd
+++ ConstructingStuff/ObjectSummoners/TreeSummoner.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var tree_scene = preload("res://ConstructingStuff/Buildings/TallTrunk.tscn")
+var EnemyOutpost = preload("res://ConstructingStuff/Buildings/TrunkWithOutpost.tscn")
 var NewTree
 onready var God = get_node("/root/LevelCreator")
 #var TreeParts = [TreeScene,EnemyOutpost]
@@ -8,22 +9,15 @@ onready var God = get_node("/root/LevelCreator")
 
 
 func _ready():
-	
+	print(global_position, "Summoner")
 	randomize()
-	if randi()%6 >= 1:
-		randomize()
+	if randi()%4 < 3:
 		NewTree = tree_scene.instance()
-		NewTree.height = God.previous_tree_height + randi()%3 - 1
-		
-		if NewTree.height < 1:
-			NewTree.height = 1
-		God.previous_tree_height = NewTree.height
-		#don't want to run into trees with negative heights. Also, want the trees to trend upwards
-		
+		if God.previous_tree_height > 1:
+			NewTree.height = God.previous_tree_height -1 + randi()%5
+		else:
+			NewTree.height = God.previous_tree_height + randi()%2
 		add_child(NewTree)
-	
-	elif God.previous_tree_height > 1:
-		God.previous_tree_height -= 1
 #		get_node("/root/LevelCreator").previous_tree_height = NewTree.height
 	
 	
